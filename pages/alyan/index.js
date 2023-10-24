fa33aaf589b9b528bc856689da7b5beb805c63ad
Arslan Ali <arslan.neni23@gmail.com>
2023-10-24 18:40:50 +0500

    changes made by me

---
+++ pages/alyan/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Alyan  ()  {
+  return (
+   <>
+   <p>Alyan Quddos</p>
+   </>
+  )
+}
+
---
+++ pages/arslan/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Arslan  ()  {
+  return (
+   <>
+   <p>Arslan</p>
+   </>
+  )
+}
+
